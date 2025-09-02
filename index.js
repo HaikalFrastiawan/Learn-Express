cd4aeedb29502526044ca9ec979f7ead9f7b8b7c
HaikalFrastiawan <haikalfrastiawan16@gmail.com>
2025-09-02 22:14:38 +0700

    fix

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const express = require("express");
 const app = express();
 
-app.use(() =>{
-    console.log("we got requsted");
-})
+app.get('/blog/:judul', (req, res) => {
+    const { judul } = req.params;
+    res.send(`Kita sedang melihat postingan dengan judul: ${judul}`);
+});
+
+app.get('/blog/:category/:judul/:author', (req, res) => {
+    const { category, judul, author } = req.params;
+    res.send(`Blog dengan kategori: ${category} | Author: ${author} | ${judul}`);
+});
+
+app.get('*', (req, res) => {
+    res.send('halaman tidak ditemukan');
+});
 
 app.listen(8080, () => {
   console.log(`Server is running on http://localhost:8080`);
