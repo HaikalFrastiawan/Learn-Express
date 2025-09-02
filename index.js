bfdebd6dbc0b1b21d6c0118fc01d94ae797df62e
HaikalFrastiawan <haikalfrastiawan16@gmail.com>
2025-09-02 22:31:36 +0700

    fix-2

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 
 app.get('/blog/:judul', (req, res) => {
@@ -10,6 +10,18 @@ app.get('/blog/:category/:judul/:author', (req, res) => {
     const { category, judul, author } = req.params;
     res.send(`Blog dengan kategori: ${category} | Author: ${author} | ${judul}`);
 });
+
+
+app.get('/search', (req, res) => {
+    const { q } = req.query;
+
+    if (!q) {
+        return res.send(`<h1>Tidak ada data yang dicari</h1>`);
+    }
+
+    res.send(`<h1>Search keyword: ${q}</h1>`);
+});
+
 
 app.get('*', (req, res) => {
     res.send('halaman tidak ditemukan');
